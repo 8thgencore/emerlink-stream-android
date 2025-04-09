d3e44477a4528137acd566667957ac80b2dee088
8thgencore <axelmaker@ya.ru>
2025-04-09 10:58:51 +0300

    ⬆️ Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "net.emerlink.stream"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.2.2"
+        versionCode = 4
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
